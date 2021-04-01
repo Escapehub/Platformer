cdae508627af8a51d4c840fb6e9917ea814d1fa8
EscapeHub <callum_speirs@protonmail.com>
2021-04-01 08:32:37 +0100

    Updated enemy

---
+++ src/enemy/enemy.cpp
@@ -0,0 +1,65 @@
+#include "enemy.h"
+
+Enemy::Enemy(EnemyHelper::EnemyType et)
+{
+    this->m_enemyHelper = std::make_unique<EnemyHelper>(et);
+    this->setOrigin(sf::Vector2f(48 / 2, 48 / 2));
+    this->m_enemyRect = sf::IntRect(0, 0, 48, 48);
+    this->setTextureRect(this->m_enemyRect);
+    this->updateEnemyAnimation(ENEMY_ANIMATION_IDLE);
+}
+
+void Enemy::drawEnemy(sf::RenderWindow& window)
+{
+    // Set player direction
+    switch (this->m_currentDirection)
+    {
+        case ENEMY_DIRECTION_RIGHT:
+            this->setScale(sf::Vector2f(1, 1));
+            break;
+
+        case ENEMY_DIRECTION_LEFT:
+            this->setScale(sf::Vector2f(-1, 1));
+            break;
+    }
+
+    // Update player texture rect for animations
+    if (this->m_clock.getElapsedTime().asSeconds() > 0.2)
+    {
+        if (this->m_enemyRect.left == this->getCurrentEndRect())
+            this->m_enemyRect.left = 0;
+        else
+            this->m_enemyRect.left += 48;
+
+        this->setTextureRect(this->m_enemyRect);
+        this->m_clock.restart();
+    }
+
+    // Drawing player sprite
+    window.draw(*this);
+}
+
+void Enemy::moveEnemy(Direction direction)
+{
+    this->m_currentDirection = direction;
+    
+    this->updateEnemyAnimation(ENEMY_ANIMATION_WALK);
+    switch (this->m_currentDirection)
+    {
+    case ENEMY_DIRECTION_RIGHT:
+        this->move(ENEMY_MOVE_SPEED, 0);
+        break;
+
+    case ENEMY_DIRECTION_LEFT:
+        this->move(-ENEMY_MOVE_SPEED, 0);
+        break;
+    }
+}
+
+void Enemy::updateEnemyAnimation(EnemyHelper::Animations ani)
+{
+    this->m_enemyHelper->setState(ani);
+    // Set player texture
+    this->m_enemyHelper->getTexture(&this->m_enemyTexture);
+    this->setTexture(this->m_enemyTexture);
+}
---
+++ src/enemy/enemy.h
@@ -0,0 +1,38 @@
+#ifndef __ENEMY__
+#define __ENEMY__
+
+#include "../enemyhelper/enemyhelper.h"
+
+#define ENEMY_DIRECTION_LEFT    Enemy::Direction::Left
+#define ENEMY_DIRECTION_RIGHT   Enemy::Direction::Right
+
+#define ENEMY_MOVE_SPEED  0.2
+
+class Enemy : public sf::Sprite
+{
+public:
+    enum Direction
+    {
+        Left, Right
+    };
+
+private:
+    std::unique_ptr<EnemyHelper> m_enemyHelper;
+    Direction m_currentDirection = ENEMY_DIRECTION_LEFT;
+    sf::Clock m_clock;
+    sf::IntRect m_enemyRect;
+    sf::Texture m_enemyTexture;
+
+private:
+    int getCurrentEndRect();
+
+public:
+    Enemy(EnemyHelper::EnemyType);
+    void drawEnemy(sf::RenderWindow&);
+
+    void moveEnemy(Direction);
+
+    void updateEnemyAnimation(EnemyHelper::Animations ani);
+};
+
+#endif
