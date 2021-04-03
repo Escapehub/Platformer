b5c8f01c9ac326f15d8139a90c287d4003462c93
EscapeHub <callum_speirs@protonmail.com>
2021-04-03 11:34:30 +0100

    moved event into helper

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "src/levelgen/levelgen.h"
 #include "src/player/player.h"
+#include "src/helpers/helpers.hpp"
 
 int main()
 {
@@ -27,61 +28,7 @@ int main()
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                window.close();
-
-            switch (event.type)
-            {
-                case sf::Event::KeyReleased:
-                    switch (event.key.code)
-                    {
-                        case sf::Keyboard::Right:
-                        case sf::Keyboard::Left:
-                            player->setState(PLAYER_STATE_IDLE);
-                            break;
-
-                        case sf::Keyboard::LShift:
-                            player->setIsRunning(false);
-                            break;
-
-                        default:
-                            break;
-                    }
-                    break;
-
-                case sf::Event::KeyPressed:
-                    switch (event.key.code)
-                    {
-                        case sf::Keyboard::LShift:
-                            player->setIsRunning(true);
-                        default:
-                            break;
-
-                        case sf::Keyboard::A:
-                            player->setState(PLAYER_STATE_ATTACK1);
-                            break;
-                        case sf::Keyboard::S:
-                            player->setState(PLAYER_STATE_ATTACK2);
-                            break;
-                        case sf::Keyboard::D:
-                            player->setState(PLAYER_STATE_ATTACK3);
-                            break;
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        // Player actions
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            player->movePlayer(PLAYER_DIRECTION_RIGHT);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            player->movePlayer(PLAYER_DIRECTION_LEFT);
+        RegisterEvent(window, player);
         
         window.clear();
         window.draw(map);
